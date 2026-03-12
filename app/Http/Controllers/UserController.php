d7f6afef64e8ab6e5b48865460f8d4ab42b8f34b
Manuel Diaz <102185837+ManuDev01@users.noreply.github.com>
2026-03-12 19:34:06 -0400

    Implement getAll method and fix constructor
    Fix constructor method and add getAll function to retrieve users.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,9 +1,20 @@
 <?php
 
   use app/Services/UserService;
+    use Illuminate\Http\Request;
 
   Class UserController {
+    protected $users;
 
+    public function __contruct(UserService $UserService) {
+        $this->users;
+    }
+
+    public function getAll(){
+        $allUsers = $this->users->listAll();
+    
+        return response() -> json (['data' => $allUsers], 200);
+    }
     
   }
 ?>
