c0ccf1c18a2fa531822c8ae77738abfd4d7f4cd7
Manuel Diaz <102185837+ManuDev01@users.noreply.github.com>
2026-03-12 19:28:38 -0400

    Add listAll method to UserService for product listing

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,8 +1,15 @@
 <?php
 
+    namespace App\Services;
+
+    use Illuminate\Support\Facades\DB;
+
   class UserService {
   
-  
+  public function listAll(){
+      return DB::select("SELECT * FROM products");
+  }
+      
     
   }
 
