fc8f6c09288fc8d70b2e482d96c8a2d39d090e74
SHZ2608 <santiagoh2608@gmail.com>
2026-03-17 18:04:39 -0400

    acordate de cambiar esto santi

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use app\controller\UserController;
 
 Route::get('/getAllUsers', [UserController::class, 'index']);
 
+Route::get('/getAllProducts');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
